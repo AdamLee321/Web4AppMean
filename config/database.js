82a3e32ff711a4a343e96e05ad5fafd1d0640c19
s00174787 <adam123dublin@gmail.com>
2017-04-20 14:09:24 +0100

    Linked DB

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //database: "mongodb://root:admin@ds163020.mlab.com:63020/web4mean",
-    database: "mongodb://localhost:27017/meandb",
+    database: "mongodb://root:admin@ds163020.mlab.com:63020/web4mean",
+    //database: "mongodb://localhost:27017/meandb",
     secret: 'mysecret'
 }
